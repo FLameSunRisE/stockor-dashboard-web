511119281de0d0aa9552d8413b4d81bdf898d58b
FLameSunRisE <flamesunrises@gmail.com>
2022-08-12 17:00:34 +0800

    feat: tailwindcss init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <h1 class="text-3xl font-bold underline">Hello tailwindcss!</h1>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -30,11 +23,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
