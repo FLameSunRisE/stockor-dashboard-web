178f955861462fffc13a1f17561753efd022a574
FLameSunRisE <flamesunrises@gmail.com>
2022-08-14 18:26:32 +0800

    fix: deploy sh file

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,9 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
+git checkout main
 git add -A
-git commit -m 'deploy : github page deploy'
+git commit -m 'deploy'
 # 部署到 分支為 gh-pages
 git push -f https://github.com/FLameSunRisE/stockor-dashboard-web.git main:gh-pages
 
